ce61f5651646f54cf2101290d6d81c324d396c57
Aaron Boushley <boushley@gmail.com>
2015-03-26 15:28:44 -0700

    Find closest source to creep.

--- out/harvest.js
+++ out/harvest.js
@@ -3,7 +3,7 @@ module.exports = exports = function (creep) {
     if (!isViable(creep)) {
         creep.suicide();
     } else if (creep.energy < creep.energyCapacity) {
-        var source = spawn.pos.findClosest(Game.SOURCES);
+        var source = creep.pos.findClosest(Game.SOURCES);
         if (source) {
             creep.moveTo(source);
             creep.harvest(source);
