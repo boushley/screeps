ad97dedb1dc14300b05e36e21ce3e37eebbfb380
Aaron Boushley <boushley@gmail.com>
2015-03-26 19:18:31 -0700

    Update build method with better fallback.

--- out/build.js
+++ out/build.js
@@ -1,17 +1,22 @@
 var harvest = require('harvest');
 
 module.exports = exports = function(creep) {
-    if (creep.energy === 0) {
-        creep.moveTo(Game.spawns.Spawn1);
-        Game.spawns.Spawn1.transferEnergy(creep);
-    }
-    else {
-        var target = creep.pos.findClosest(Game.CONSTRUCTION_SITES);
-        if (target) {
+    var spawn = Game.spawns[creep.memory.spawnName];
+    var target = spawn.pos.findClosest(Game.CONSTRUCTION_SITES, {
+        filter: function(site) {
+            return site.my;
+        }
+    });
+
+    if (target) {
+        if (creep.energy === 0) {
+            creep.moveTo(spawn);
+            spawn.transferEnergy(creep);
+        } else {
             creep.moveTo(target);
             creep.build(target);
-        } else {
-            harvest(creep);
         }
+    } else {
+        harvest(creep);
     }
 };
