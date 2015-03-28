6d7cb6da14aedb24f1656253fc4f7816ff19e75f
Aaron Boushley <boushley@gmail.com>
2015-03-28 13:53:42 -0700

    Update builder logic to include repair.

--- out/build.js
+++ out/build.js
@@ -2,22 +2,39 @@
 
 var harvest = require("harvest");
 
+function handleTarget(creep, spawn, target, isRepair) {
+    if (creep.energy === 0) {
+        creep.moveTo(spawn);
+        if (spawn.energy > 200) {
+            spawn.transferEnergy(creep);
+        }
+    } else {
+        creep.moveTo(target);
+        if (isRepair) {
+            creep.repair(target);
+        } else {
+            creep.build(target);
+        }
+    }
+}
+
 module.exports = exports = function (creep) {
     var spawn = Game.spawns[creep.memory.spawnName];
-    var target = spawn.pos.findClosest(Game.CONSTRUCTION_SITES, {
-        filter: function filter(site) {
-            return site.my;
+    var buildTarget = spawn.pos.findClosest(Game.CONSTRUCTION_SITES, {
+        filter: function (s) {
+            return s.my;
+        }
+    });
+    var repairTarget = spawn.pos.findClosest(Game.MY_STRUCTURES, {
+        filter: function (s) {
+            return s.hits < s.hitsMax - 20;
         }
     });
 
-    if (target) {
-        if (creep.energy === 0) {
-            creep.moveTo(spawn);
-            spawn.transferEnergy(creep);
-        } else {
-            creep.moveTo(target);
-            creep.build(target);
-        }
+    if (buildTarget) {
+        handleTarget(creep, spawn, buildTarget, false);
+    } else if (repairTarget) {
+        handleTarget(creep, spawn, repairTarget, true);
     } else {
         harvest(creep);
     }
--- src/build.js
+++ src/build.js
@@ -2,22 +2,35 @@
 
 var harvest = require('harvest');
 
-module.exports = exports = function(creep) {
-    var spawn = Game.spawns[creep.memory.spawnName];
-    var target = spawn.pos.findClosest(Game.CONSTRUCTION_SITES, {
-        filter: function(site) {
-            return site.my;
-        }
-    });
-
-    if (target) {
-        if (creep.energy === 0) {
-            creep.moveTo(spawn);
+function handleTarget(creep, spawn, target, isRepair) {
+    if (creep.energy === 0) {
+        creep.moveTo(spawn);
+        if (spawn.energy > 200) {
             spawn.transferEnergy(creep);
+        }
+    } else {
+        creep.moveTo(target);
+        if (isRepair) {
+            creep.repair(target);
         } else {
-            creep.moveTo(target);
             creep.build(target);
         }
+    }
+}
+
+module.exports = exports = function(creep) {
+    var spawn = Game.spawns[creep.memory.spawnName];
+    var buildTarget = spawn.pos.findClosest(Game.CONSTRUCTION_SITES, {
+        filter: s => s.my
+    });
+    var repairTarget = spawn.pos.findClosest(Game.MY_STRUCTURES, {
+        filter: s => s.hits < (s.hitsMax - 20)
+    });
+
+    if (buildTarget) {
+        handleTarget(creep, spawn, buildTarget, false);
+    } else if (repairTarget) {
+        handleTarget(creep, spawn, repairTarget, true);
     } else {
         harvest(creep);
     }
