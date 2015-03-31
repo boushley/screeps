c43023205b337d2a76a9da1489eeb425b2e38aac
Aaron Boushley <boushley@gmail.com>
2015-03-31 10:25:25 -0700

    Have 1:1 healer to guard ratio.
    Also allow healers to heal and ranged heal in single turn.

--- src/goal-close-guards.js
+++ src/goal-close-guards.js
@@ -5,6 +5,8 @@ let GoalBase = require('goal-base'),
     Guard = require('guard'),
     Healer = require('healer');
 
+const ADVANCED_UNIT_THRESHOLD = 1000;
+
 class GoalCloseGuards extends GoalBase {
     static key() {
         return 'goal-close-guards';
@@ -14,27 +16,48 @@ class GoalCloseGuards extends GoalBase {
         return false;
     }
 
+    getHealerToBuild() {
+        let healerCount = counts[Healer.key()] || 0,
+            parts;
+
+        if (this.spawn.energy > ADVANCED_UNIT_THRESHOLD) {
+            parts = [Game.TOUGH, Game.MOVE, Game.HEAL, Game.MOVE, Game.HEAL];
+        } else if (healerCount < 1) {
+            parts = [Game.MOVE, Game.HEAL];
+        } else {
+            parts = [Game.MOVE, Game.HEAL, Game.MOVE, Game.HEAL];
+        }
+
+        return parts;
+    }
+
+    getGuardToBuild() {
+        let guardCount = counts[Guard.key()] || 0,
+            parts;
+
+        if (this.spawn.energy > ADVANCED_UNIT_THRESHOLD) {
+            parts = [Game.TOUGH, Game.TOUGH, Game.RANGED_ATTACK, Game.MOVE, Game.RANGED_ATTACK, Game.MOVE];
+        } else if (guardCount < 1) {
+            parts = [Game.RANGED_ATTACK, Game.MOVE];
+        } else {
+            parts = [Game.TOUGH, Game.TOUGH, Game.RANGED_ATTACK, Game.MOVE];
+        }
+
+        return parts;
+    }
+
     getCreepToBuild() {
         let parts,
             role,
             guardCount = counts[Guard.key()] || 0,
             healerCount = counts[Healer.key()] || 0;
 
-        if (guardCount < 1) {
-            role = Guard.key();
-            parts = [Game.RANGED_ATTACK, Game.MOVE];
-        } else if (healerCount < 1) {
+        if (healerCount < guardCount) {
             role = Healer.key();
-            parts = [Game.MOVE, Game.HEAL];
-        } else if (guardCount < 2) {
-            role = Guard.key();
-            parts = [Game.RANGED_ATTACK, Game.MOVE];
-        } else if (healerCount < 2) {
-            role = Healer.key();
-            parts = [Game.MOVE, Game.HEAL, Game.MOVE, Game.HEAL];
+            parts = this.getHealerToBuild();
         } else {
             role = Guard.key();
-            parts = [Game.TOUGH, Game.TOUGH, Game.RANGED_ATTACK, Game.MOVE];
+            parts = this.getGuardToBuild();
         }
 
         return {
@@ -47,4 +70,3 @@ class GoalCloseGuards extends GoalBase {
 }
 
 module.exports = GoalCloseGuards;
-
--- src/healer.js
+++ src/healer.js
@@ -8,27 +8,21 @@ class Healer extends BaseRole {
     }
 
     run() {
-        var target = this.creep.pos.findClosest(Game.MY_CREEPS, {
-            filter: function(object) {
-                return object.hits < object.hitsMax;
-            }
-        });
+        let target = this.creep.pos.findClosest(Game.MY_CREEPS, {
+                filter: c => c.hits < c.hitsMax
+            }),
+            moveTarget = target || this.getRally();
 
-        // TODO Take advantage of the fact that we can heal AND
-        // ranged heal AND move in the same turn.
-        // Also have better standby logic, maybe something to
-        // stay away from enemies.
         if (target) {
             if (this.creep.pos.isNearTo(target)) {
                 this.creep.heal(target);
-            } else if (this.creep.pos.inRangeTo(target, 3)) {
-                this.creep.rangedHeal(target);
-            } else {
-                this.creep.moveTo(target);
             }
-        } else {
-            this.creep.moveTo(this.getRally());
+            if (this.creep.pos.inRangeTo(target, 3)) {
+                this.creep.rangedHeal(target);
+            }
         }
+
+        this.creep.moveTo(moveTarget);
     }
 }
 
